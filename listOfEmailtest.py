5484bbd02412187e634ae7239258e550847faf66
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-03-25 15:39:00 +0530

    [sachin]Add.Added program for filtering valid email id's from file

---
+++ listOfEmailtest.py
@@ -0,0 +1,20 @@
+# import urllib.request
+import re as r
+class ListOfEmailTest:
+    def __init__(self, filename):
+        #opening the file
+        self.__filename=open(filename,'r')
+
+    
+    def checkEmail(self):
+        a = []
+        email_data = self.__filename.readlines()
+        for i in email_data:
+            #checking maid id is vaid or not
+            if r.match(r'[A-Za-z]+[\.\-\+]?[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]{2}[A-Za-z]*\.?(?=[^\d])\w*\,?$',i.strip()):
+                #adding valid email id to list
+                a.append(i.strip())
+        return a
+        f.close()
+list_email = ListOfEmailTest('emails.txt').checkEmail()
+print(list_email)
