1ec6db1a939cdeda1d61a04e5bce06f92ede2eec
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-03-25 19:39:25 +0530

    [sachin]Add.Added program for filtering valid email id's from file

--- listOfEmailtest.py
+++ listOfEmailtest.py
@@ -9,6 +9,7 @@ class ListOfEmailTest:
     def checkEmail(self):
         a = []
         email_data = self.__filename.readlines()
+        # abc=['abc@yahoo.com,','abc-100@yahoo.com,','abc.100@yahoo.com','abc111@abc.com,','.abc@abc.com']
         for i in email_data:
             #checking maid id is vaid or not
             if r.match(r'[A-Za-z]+[\.\-\+]?[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]{2}[A-Za-z]*\.?(?=[^\d])\w*\,?$',i.strip()):
